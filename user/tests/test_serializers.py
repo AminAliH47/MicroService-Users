59f60addd4c570579bf5452734b75994564d0b68
AminAlih47 <AminAlih47@gmail.com>
2022-08-22 21:21:59 +0430

    Tests updated!

---
+++ user/tests/test_serializers.py
@@ -0,0 +1,30 @@
+from django.test import TestCase
+
+from user.serializers import UserProtoSerializer
+
+
+class TestSerializers(TestCase):
+    def test_user_serializer_valid_data(self):
+        """
+        Test User Proto Serializer data is valid
+        """
+        serializer = UserProtoSerializer(
+            data={
+                "username": "test1",  # required
+                "first_name": "First test1",
+                "last_name": "Last test1",
+                "email": "test1@test.com",
+                "password": "Test12345",  # required
+            }
+        )
+
+        self.assertTrue(serializer.is_valid())
+
+    def test_user_serializer_empty_data(self):
+        """
+        Test User Proto Serializer with empty data is invalid
+        """
+        serializer = UserProtoSerializer(data={})
+
+        self.assertFalse(serializer.is_valid())
+        self.assertEqual(len(serializer.errors), 2)
